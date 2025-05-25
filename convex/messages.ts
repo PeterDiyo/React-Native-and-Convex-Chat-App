aaa3c4f4282cd790ef5ec63c2dbabb63be3f8ba5
PETER DIYO <princediyo93@gmail.com>
2025-05-25 10:27:38 +0530

    Adding messages table in the schema, query and mutation

---
+++ convex/messages.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+// This mutation is used to create a new message in the database.
+export const sendMessage = mutation({
+  args: {
+    content: v.string(),
+    group_id: v.id("groups"),
+    user: v.string(),
+    file: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("messages", args);
+  },
+});
+
+export const get = query({
+  args: { chatId: v.id("groups") },
+  handler: async ({ db }, { chatId }) => {
+    return await db
+      .query("messages")
+      .filter((q) => q.eq(q.field("group_id"), chatId))
+      .collect();
+  },
+});
